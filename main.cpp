adef530d7e4c96b3a5a523b16338a332328edd8e
Ismael Vazquez  Jr. <ismaelvazquez182@gmail.com>
2025-08-24 09:48:12 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include "Character.h"
+#include "BHGui.h"
+#include "ProcessManager.h"
+
+ProcessManager pm(L"Souls.exe");
+Character* character = nullptr;
+
+void grow_pp()
+{
+	const static int pp_value = 100000;
+	pm.WriteInt(reinterpret_cast<DWORD>(&character->NegativePP), -pp_value);
+	pm.WriteInt(reinterpret_cast<DWORD>(&character->PP), pp_value);
+}
+
+int WINAPI wWinMain(
+	_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ PWSTR pCmdLine,
+	_In_ int nCmdShow
+)
+{
+	DWORD character_pointer;
+	if (pm.Read(reinterpret_cast<LPCVOID>(PLAYER_BASE), &character_pointer, sizeof(character_pointer)) == 0) {
+		return 1;
+	}
+
+	character = (Character*)character_pointer;
+
+	BHGui gui{ "Bobby Hill's PP Grower", 450, 200 };
+	gui.Run(grow_pp);
+	return 0;
+}
