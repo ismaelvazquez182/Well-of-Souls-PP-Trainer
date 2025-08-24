4b29c2e9d504a877c28e8ec2448811c3ad5555da
Ismael Vazquez  Jr. <ismaelvazquez182@gmail.com>
2025-08-24 09:52:53 -0700

    Create BHGui.h

---
+++ src/BHGui.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <string>
+
+class BHGui {
+
+public:
+	BHGui(std::string title, unsigned int window_width, unsigned int window_height);
+	~BHGui();
+	void Run(void (*action_function)());
+private:
+	const std::string m_window_title;
+	const unsigned int m_window_width;
+	const unsigned int m_window_height;
+
+	const std::string m_button_text{ "GROW MY PP" };
+	const unsigned int m_button_width{ 400 };
+	const unsigned int m_button_height{ 100 };
+	const unsigned int m_button_font_size{ 48 };
+};
